7bf04b5591c91b1e9cf18472b62662931b69965a
sarra ayari <sarra.ayari@aymax.fr>
2024-07-11 11:20:54 +0000

    api management

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel,MessageBox) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
@@ -30,12 +32,88 @@ sap.ui.define([
                    }
                 })
 
-                this.getOAuthToken().then((sToken) => {
-                    this.getUsers(sToken);
-                }).catch((error) => {
-                    console.error("OAuth Error:", error);
-                });
             },
+            onPress__: function () {
+                var data = {
+                    "SNDPOR": "string",
+                    "MESTYP": "string",
+                    "BEGIN": "string",
+                    "RCVPOR": "string",
+                    "MANDT": "string",
+                    "SEGMENT_1": "string",
+                    "EDI_DC40": "string",
+                    "SEGMENT_3": "string",
+                    "SEGMENT_2": "string",
+                    "MTART": "string",
+                    "E1MARAM": "string",
+                    "id": 47665,
+                    "DIRECT": "string",
+                    "MATNR": "string",
+                    "ERNAM": "string",
+                    "RCVPRN": "string",
+                    "E1MAKTM": "string",
+                    "SNDPRT": "string",
+                    "RCVPRT": "string",
+                    "MEINS": "string",
+                    "SNDPRN": "string",
+                    "MBRSH": "string",
+                    "TABNAM": "string",
+                    "IDOCTYP": "string",
+                    "CONTAINER": "string"
+                };
+
+                var apiKey = 'nUNcFBi5tzA3Zyx1ZowPfcGs8tcnyvQ5';
+            var authorization = "Basic"+"sarra.ayari@aymax.fr"+":"+"u3bmjLMnGDW3bJe";
+            
+
+            var url = "https://14385865trial-trial.integrationsuitetrial-apim.us10.hana.ondemand.com:443/14385865trial/v1/post";
+            url += '?callback=callbackName';
+            url += '&data=' + encodeURIComponent(JSON.stringify(data));
+            url += '&APIKey=' + encodeURIComponent(apiKey);
+            url += '&Authorization=' + encodeURIComponent(authorization);               
+            
+    
+
+            $.ajax({
+                url: url,
+                type: 'POST',
+                            dataType: "jsonp",
+                            jsonpCallback: "handleResponse",
+                            data : data,
+                      APIKey : 'nUNcFBi5tzA3Zyx1ZowPfcGs8tcnyvQ5',
+                      Authorization : "Basic "+"sarra.ayari@aymax.fr"+":"+"u3bmjLMnGDW3bJe",
+                            success: function(data) {console.log(data);          },
+                       
+                    
+                   error:function(oError)
+                   {
+                        console.log(oError);
+                   }
+                });
+
+                
+            },    
+
+        onClick: function () {
+
+            
+        // get selected data => dynamic function
+        $.ajax({
+            type: 'GET',
+            url: "https://14385865trial-trial.integrationsuitetrial-apim.us10.hana.ondemand.com:443/14385865trial/v1/openconnector",
+            data : 1,
+            contentType : 'application/json',
+            Authorization : `Basic sb-f1802272-ccd0-41e5-9df4-e79403f0c99a!b297017|it!b26655:351e9efe-8bf5-4bee-8172-51b67b2f99f6$_xv2TgE8YPiJePwC2nvErf9YLRziE4P7CBMYi8ooIFE=`,
+            success: function(data){
+                console.log(data);
+            },
+            error: function(oError){
+            console.log(oError);
+            }
+
+            })
+},
+
             onPress: function (){
                 var data = JSON.stringify({
                     
@@ -50,7 +128,6 @@ sap.ui.define([
                         "SEGMENT_2": "string",
                         "MTART": "string",
                         "E1MARAM": "string",
-                        "id": 4000,
                         "DIRECT": "string",
                         "MATNR": "string",
                         "ERNAM": "string",
@@ -66,15 +143,14 @@ sap.ui.define([
                         "CONTAINER": "string"
                       
                   })
+                
+
                 $.ajax({
                     type: 'POST',
-                    xhrFields: {
-                        withCredentials: true
-                    },
-                     url: "https://14385865trial.it-cpitrial05-rt.cfapps.us10-001.hana.ondemand.com/http/post",
-                     data : data ,
+                     url: "https://14385865trial-trial.integrationsuitetrial-apim.us10.hana.ondemand.com:443/14385865trial/v1/post",
+                     data : data,
                       contentType : 'application/json',
-                      Authorization : "Basic"+"sb-f9872ca1-dea0-4c2c-8a9a-b07a310044b4!b297017|it-rt-14385865trial!b26655"+":"+"123469a4-0bf1-4529-9c08-6b7c28adb98d$Rk2yuWaqM_uaQd-28Cr34wpYW1GEQlbOe18D0DfN2Yo="                     ,
+                       Authorization : `Basic sb-f1802272-ccd0-41e5-9df4-e79403f0c99a!b297017|it!b26655:351e9efe-8bf5-4bee-8172-51b67b2f99f6$_xv2TgE8YPiJePwC2nvErf9YLRziE4P7CBMYi8ooIFE=`,
                     success: function(data){
                          console.log(data);
                      },
